6e328fb8e8347fc132cd9a9b40fd1a5ad98e9796
ethanMilne <ethanmilne@sheridan.edu>
2024-12-11 01:53:00 +0000

    working on puzzle 2

--- day2/puzzle.py
+++ day2/puzzle.py
@@ -23,54 +23,73 @@ def fileRunner(fileName):
 
         #iterate through each list and checking how it compares to the rules
         #having different possibilities based on the length of the list :/
-        if listLength == 5:
+        safetyCount = isSafe(lineList)
 
-            if ((lineList[0] > lineList[1] and lineList[1] > lineList[2] and lineList[2] > lineList[3] and lineList[3] > lineList[4]) or
-                (lineList[0] < lineList[1] and lineList[1] < lineList[2] and lineList[2] < lineList[3] and lineList[3] < lineList[4])):
-                #This crazy if statement tells if the difference between the levels is at least one and at most three
-                if(((abs(lineList[0] - lineList[1]) >= 1 and abs(lineList[0] - lineList[1]) <= 3) and 
-                    (abs(lineList[1] - lineList[2]) >= 1 and abs(lineList[1] - lineList[2]) <= 3) and 
-                    (abs(lineList[2] - lineList[3]) >= 1 and abs(lineList[2] - lineList[3]) <= 3) and 
-                    (abs(lineList[3] - lineList[4]) >= 1 and abs(lineList[3] - lineList[4]) <= 3))):
-                    safetyCount += 1
-
-        elif listLength == 6:
-            if ((lineList[0] > lineList[1] and lineList[1] > lineList[2] and lineList[2] > lineList[3] and lineList[3] > lineList[4] and lineList[4] > lineList[5]) or
-                (lineList[0] < lineList[1] and lineList[1] < lineList[2] and lineList[2] < lineList[3] and lineList[3] < lineList[4] and lineList[4] < lineList[5])):
-                if(((abs(lineList[0] - lineList[1]) >= 1 and abs(lineList[0] - lineList[1]) <= 3) and 
-                    (abs(lineList[1] - lineList[2]) >= 1 and abs(lineList[1] - lineList[2]) <= 3) and 
-                    (abs(lineList[2] - lineList[3]) >= 1 and abs(lineList[2] - lineList[3]) <= 3) and 
-                    (abs(lineList[3] - lineList[4]) >= 1 and abs(lineList[3] - lineList[4]) <= 3) and 
-                    (abs(lineList[4] - lineList[5]) >= 1 and abs(lineList[4] - lineList[5]) <= 3))):
-                    safetyCount += 1
-
-        elif listLength == 7:
-            if ((lineList[0] > lineList[1] and lineList[1] > lineList[2] and lineList[2] > lineList[3] and lineList[3] > lineList[4] and lineList[4] > lineList[5] and lineList[5] > lineList[6]) or
-                (lineList[0] < lineList[1] and lineList[1] < lineList[2] and lineList[2] < lineList[3] and lineList[3] < lineList[4] and lineList[4] < lineList[5] and lineList[5] < lineList[6])):
-                if(((abs(lineList[0] - lineList[1]) >= 1 and abs(lineList[0] - lineList[1]) <= 3) and 
-                    (abs(lineList[1] - lineList[2]) >= 1 and abs(lineList[1] - lineList[2]) <= 3) and 
-                    (abs(lineList[2] - lineList[3]) >= 1 and abs(lineList[2] - lineList[3]) <= 3) and 
-                    (abs(lineList[3] - lineList[4]) >= 1 and abs(lineList[3] - lineList[4]) <= 3) and 
-                    (abs(lineList[4] - lineList[5]) >= 1 and abs(lineList[4] - lineList[5]) <= 3) and 
-                    (abs(lineList[5] - lineList[6]) >= 1 and abs(lineList[5] - lineList[6]) <= 3))):
-                    safetyCount += 1
-
-        elif listLength == 8:
-            if ((lineList[0] > lineList[1] and lineList[1] > lineList[2] and lineList[2] > lineList[3] and lineList[3] > lineList[4] and lineList[4] > lineList[5] and lineList[5] > lineList[6] and lineList[6] > lineList[7]) or
-                (lineList[0] < lineList[1] and lineList[1] < lineList[2] and lineList[2] < lineList[3] and lineList[3] < lineList[4] and lineList[4] < lineList[5] and lineList[5] < lineList[6] and lineList[6] < lineList[7])):
-                if(((abs(lineList[0] - lineList[1]) >= 1 and abs(lineList[0] - lineList[1]) <= 3) and 
-                    (abs(lineList[1] - lineList[2]) >= 1 and abs(lineList[1] - lineList[2]) <= 3) and 
-                    (abs(lineList[2] - lineList[3]) >= 1 and abs(lineList[2] - lineList[3]) <= 3) and 
-                    (abs(lineList[3] - lineList[4]) >= 1 and abs(lineList[3] - lineList[4]) <= 3) and 
-                    (abs(lineList[4] - lineList[5]) >= 1 and abs(lineList[4] - lineList[5]) <= 3) and 
-                    (abs(lineList[5] - lineList[6]) >= 1 and abs(lineList[5] - lineList[6]) <= 3) and 
-                    (abs(lineList[6] - lineList[7]) >= 1 and abs(lineList[6] - lineList[7]) <= 3))):
-                    safetyCount += 1
 
 
     file.close()
 
     return safetyCount
+
+
+def isSafe(lineList):
+
+    safetyCount = 0
+    listLength = len(lineList)
+    if listLength == 4:
+        if ((lineList[0] > lineList[1] and lineList[1] > lineList[2] and lineList[2] > lineList[3]) or
+            (lineList[0] < lineList[1] and lineList[1] < lineList[2] and lineList[2] < lineList[3])):
+            #This crazy if statement tells if the difference between the levels is at least one and at most three
+            if(((abs(lineList[0] - lineList[1]) >= 1 and abs(lineList[0] - lineList[1]) <= 3) and 
+                (abs(lineList[1] - lineList[2]) >= 1 and abs(lineList[1] - lineList[2]) <= 3) and 
+                (abs(lineList[2] - lineList[3]) >= 1 and abs(lineList[2] - lineList[3]) <= 3))):
+                    safetyCount += 1
+    
+    elif listLength == 5:
+        if ((lineList[0] > lineList[1] and lineList[1] > lineList[2] and lineList[2] > lineList[3] and lineList[3] > lineList[4]) or
+            (lineList[0] < lineList[1] and lineList[1] < lineList[2] and lineList[2] < lineList[3] and lineList[3] < lineList[4])):
+            #This crazy if statement tells if the difference between the levels is at least one and at most three
+            if(((abs(lineList[0] - lineList[1]) >= 1 and abs(lineList[0] - lineList[1]) <= 3) and 
+                (abs(lineList[1] - lineList[2]) >= 1 and abs(lineList[1] - lineList[2]) <= 3) and 
+                (abs(lineList[2] - lineList[3]) >= 1 and abs(lineList[2] - lineList[3]) <= 3) and 
+                (abs(lineList[3] - lineList[4]) >= 1 and abs(lineList[3] - lineList[4]) <= 3))):
+                    safetyCount += 1
+
+    elif listLength == 6:
+        if ((lineList[0] > lineList[1] and lineList[1] > lineList[2] and lineList[2] > lineList[3] and lineList[3] > lineList[4] and lineList[4] > lineList[5]) or
+            (lineList[0] < lineList[1] and lineList[1] < lineList[2] and lineList[2] < lineList[3] and lineList[3] < lineList[4] and lineList[4] < lineList[5])):
+            if(((abs(lineList[0] - lineList[1]) >= 1 and abs(lineList[0] - lineList[1]) <= 3) and 
+                (abs(lineList[1] - lineList[2]) >= 1 and abs(lineList[1] - lineList[2]) <= 3) and 
+                (abs(lineList[2] - lineList[3]) >= 1 and abs(lineList[2] - lineList[3]) <= 3) and 
+                (abs(lineList[3] - lineList[4]) >= 1 and abs(lineList[3] - lineList[4]) <= 3) and 
+                (abs(lineList[4] - lineList[5]) >= 1 and abs(lineList[4] - lineList[5]) <= 3))):
+                    safetyCount += 1
+    
+    elif listLength == 7:
+        if ((lineList[0] > lineList[1] and lineList[1] > lineList[2] and lineList[2] > lineList[3] and lineList[3] > lineList[4] and lineList[4] > lineList[5] and lineList[5] > lineList[6]) or
+            (lineList[0] < lineList[1] and lineList[1] < lineList[2] and lineList[2] < lineList[3] and lineList[3] < lineList[4] and lineList[4] < lineList[5] and lineList[5] < lineList[6])):
+            if(((abs(lineList[0] - lineList[1]) >= 1 and abs(lineList[0] - lineList[1]) <= 3) and 
+                (abs(lineList[1] - lineList[2]) >= 1 and abs(lineList[1] - lineList[2]) <= 3) and 
+                (abs(lineList[2] - lineList[3]) >= 1 and abs(lineList[2] - lineList[3]) <= 3) and 
+                (abs(lineList[3] - lineList[4]) >= 1 and abs(lineList[3] - lineList[4]) <= 3) and 
+                (abs(lineList[4] - lineList[5]) >= 1 and abs(lineList[4] - lineList[5]) <= 3) and 
+                (abs(lineList[5] - lineList[6]) >= 1 and abs(lineList[5] - lineList[6]) <= 3))):
+                    safetyCount += 1
+
+    elif listLength == 8:
+        if ((lineList[0] > lineList[1] and lineList[1] > lineList[2] and lineList[2] > lineList[3] and lineList[3] > lineList[4] and lineList[4] > lineList[5] and lineList[5] > lineList[6] and lineList[6] > lineList[7]) or
+            (lineList[0] < lineList[1] and lineList[1] < lineList[2] and lineList[2] < lineList[3] and lineList[3] < lineList[4] and lineList[4] < lineList[5] and lineList[5] < lineList[6] and lineList[6] < lineList[7])):
+            if(((abs(lineList[0] - lineList[1]) >= 1 and abs(lineList[0] - lineList[1]) <= 3) and 
+                (abs(lineList[1] - lineList[2]) >= 1 and abs(lineList[1] - lineList[2]) <= 3) and 
+                (abs(lineList[2] - lineList[3]) >= 1 and abs(lineList[2] - lineList[3]) <= 3) and 
+                (abs(lineList[3] - lineList[4]) >= 1 and abs(lineList[3] - lineList[4]) <= 3) and 
+                (abs(lineList[4] - lineList[5]) >= 1 and abs(lineList[4] - lineList[5]) <= 3) and 
+                (abs(lineList[5] - lineList[6]) >= 1 and abs(lineList[5] - lineList[6]) <= 3) and 
+                (abs(lineList[6] - lineList[7]) >= 1 and abs(lineList[6] - lineList[7]) <= 3))):
+                    safetyCount += 1
+    
+    return safetyCount
+
 
 
 
