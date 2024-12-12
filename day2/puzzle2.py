8246a0a85718f08ace102a354605f129d6b7945d
ethanMilne <ethanmilne@sheridan.edu>
2024-12-12 23:45:53 +0000

    continued work on day 2

--- day2/puzzle2.py
+++ day2/puzzle2.py
@@ -6,6 +6,12 @@
 #   any two adjacent levels differ by at least one and at most three
 #use elif tree to check if each respective list follows the rules
 
+#now on part 2!
+#problem dampener can remove one index from each list
+#if removing that index means the list is safe, the list now becomes safe
+#create a function that iterates through a list and removes each sequential index
+#while calling isSafe() on each runthrough to see if the new list is safe
+
 def fileRunner(fileName):
     print("run your files with this simple trick")
     #count how many lists are safe
@@ -22,9 +28,9 @@ def fileRunner(fileName):
             lineList[item] = int(lineList[item])
 
         #iterate through each list and checking how it compares to the rules
-        levelIsSafe = isSafe(lineList)
-        if(levelIsSafe):
-            safetyCount += 1
+        # levelIsSafe = isSafe(lineList)
+        levelIsSafe = removeLevels(lineList)
+        safetyCount += levelIsSafe
 
 
 
@@ -57,7 +63,34 @@ def isSafe(lineList):
             break
     return safety
          
+def removeLevels(lineList):
 
+    levelCounter = 0 #to count how many safe levels there are
+    originalTest = isSafe(lineList)# if a level is safe, we don't need to iterate through and remove levels
+    if (not originalTest):
+        #sequentially remove each index in lineList and test whether isSafe()
+        # print("Now you're removing levels")
+        lineLength = len(lineList)
+        for index in range(lineLength):
+
+            newLineList = []
+            for listIndex in lineList: #copy into temp variable so actual list doesn't get changed
+                newLineList.append(listIndex)
+
+            print("This is the original line:", lineList)
+            print("This is the copied line:", newLineList)
+            newLineList.remove(newLineList[index])
+            # print("This is the original line:", lineList)
+            print("This is the changed line:", newLineList)
+            boolean = isSafe(newLineList)
+            if boolean:
+                levelCounter += 1
+                break
+    else:# if the original level was actually just safe
+        # add one to the level counter and move on
+        levelCounter += 1
+
+    return levelCounter
 
 
 
