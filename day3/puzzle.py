c6748a2d3b6199a0aabcbcd86b88c25559d3bd4d
ethanMilne <ethanmilne@sheridan.edu>
2024-12-13 04:37:50 +0000

    looked at puzzle three

---
+++ day3/puzzle.py
@@ -0,0 +1,4 @@
+#this puzzle takes a lot of text that is mostly garbage but has some useful information
+#iterate through the texts and find the useful information
+#mul(3,4) is what we're looking for
+#get rid of all spaces, words, and special characters
